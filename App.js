6b1a7880ab4fe4a6092668179fb10b1cd0293a6f
Joe Hernandez <hernandezhjoe@gmail.com>
2020-12-04 13:11:33 -0800

    setup Redux Persist

--- App.js
+++ App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import Main from './components/MainComponent';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import Loading from './components/LoadingComponent';
 
-const store = ConfigureStore();
+const { persistor, store } = ConfigureStore();
 
 export default function App() {
     return (
         <Provider store={store}>
-            <Main />
+            <PersistGate
+                loading={<Loading />}
+                persistor={persistor}>
+                <Main />
+            </PersistGate>
         </Provider>
     );
 }
 
-// passing the store to the provider component as prop this gives the main component and all of its child components the ability to connect to redux store
